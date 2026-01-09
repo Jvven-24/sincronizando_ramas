d8dbf1584a353d0b613e010a0d3d2c9e25156e04
jeven <jeven@LAPTOP-E2SVEJ83>
2026-01-09 17:39:19 -0400

    Agregar archivos de proyecto.

---
+++ capa_negocio/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace capa_negocio
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ sincronizando_ramas/Form1.cs
@@ -0,0 +1,10 @@
+namespace sincronizando_ramas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
